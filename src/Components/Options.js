395fb28d653aecda27ec4e13574ed5d0266298d6
Grant Beard <Grantxbeard@gmail.com>
2022-08-27 14:07:12 -0600

    create basic button styling

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -4,7 +4,12 @@ import "../Styles/global.scss"
 const Options = () => {
     return (
         <section className="options-container">
-            <h2>BUTTON</h2>
+            <button>Test1</button>
+            <button>Test2</button>
+            <button>Test3</button>
+            <button>Test4</button>
+            <button>Test5</button>
+            <button>Test6</button>
         </section>
     )
 }
