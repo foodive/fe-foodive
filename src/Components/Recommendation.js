1ae322cc26cb646d196dfb574b9c84804b45f9e4
Lourdes Benites <lourdesben95@gmail.com>
2022-08-28 13:40:03 -0600

    Create basic outline of recommendation

--- src/Components/Recommendation.js
+++ src/Components/Recommendation.js
@@ -3,7 +3,14 @@ import React from "react";
 
 const Recommendation = () => {
   return (
-    <p>Howdy World</p>
+    <section className="recommendation">
+      <h2>Name of restaurant</h2>
+      <section className="information">
+      </section>
+      <section className="map-container">
+
+      </section>
+    </section>
   )
 }
 
