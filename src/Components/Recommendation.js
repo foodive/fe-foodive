b180fe9d4a9a5390ef9f3959e25c8fc679763335
Lourdes Benites <lourdesben95@gmail.com>
2022-08-28 13:46:43 -0600

    Add address and properties container

--- src/Components/Recommendation.js
+++ src/Components/Recommendation.js
@@ -6,6 +6,16 @@ const Recommendation = () => {
     <section className="recommendation">
       <h2>Name of restaurant</h2>
       <section className="information">
+        <div>
+          <p>Image will go here</p>
+        </div>
+       <section className="details">
+        <section className="properties">
+
+        </section>
+        <p className="address">12345 Lala Land</p>
+       </section> 
+
       </section>
       <section className="map-container">
 
