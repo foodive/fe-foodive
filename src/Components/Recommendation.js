8cef8aaf6292dbbf0a2e31854c253c6674ebc1b4
Lourdes Benites <lourdesben95@gmail.com>
2022-08-28 14:01:39 -0600

    Add rest of restaurant details

--- src/Components/Recommendation.js
+++ src/Components/Recommendation.js
@@ -6,21 +6,29 @@ const Recommendation = () => {
     <section className="recommendation">
       <h2>Name of restaurant</h2>
       <section className="information">
-        <div>
+        <div className="restaurant-image">
           <p>Image will go here</p>
         </div>
        <section className="details">
         <section className="properties">
-
+          <div className="ratings">
+            <p>Ratings will go here</p>
+          </div>
+          <p className="cuisine">Cuisine type</p>
+          <p className="cost">Cost will go here</p>
+          <div className="phone-container">
+            <p className="phone-icon">📞</p>
+            <p className="phone-number">Phone number will go here</p>
+          </div>
         </section>
         <p className="address">12345 Lala Land</p>
        </section> 
-
       </section>
       <section className="map-container">
 
       </section>
     </section>
+
   )
 }
 
