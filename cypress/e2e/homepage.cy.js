48bfdea56ea42eaf77dcc17ac3174dd5357b30f9
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-08-29 14:21:55 -0600

    Add test to visit recommendation page

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,5 +1,11 @@
 describe('homepage flow', () => {
   it('can visit the homepage', () => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it('can visit the recommendation page when the search button is clicked', () => {
     cy.visit("http://localhost:3000/")
-  })
-})
+      .get('.submit').click()
+      .url().should('eq', 'http://localhost:3000/recommendation');
+  });
+});
