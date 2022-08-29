30ba5c5d02f5c442f0d207696b4fdd98fd54be63
Dylan Duke <laytonmaes@gmail.com>
2022-08-29 17:41:46 -0600

    add basic styling for reccommendations  page, add new mixins, components

--- src/Components/Recommendation.js
+++ src/Components/Recommendation.js
@@ -6,41 +6,49 @@ const Recommendation = ({ restaurantData, setRestaurantData }) => {
     return <p>error</p>;
   } else {
     return (
-      <section className="recommendation">
+      <main>
         <section className="info-panel">
-          <h2>{restaurantData.name}</h2>
+          <header className="info-header">
+          <h2 className="name">{restaurantData.name}</h2>
+          </header>
           <section className="information">
             <div className="restaurant-image">
-              <p>{restaurantData.image_url}</p>
+              <img src={restaurantData.image_url}></img>
             </div>
             <section className="details">
               <section className="properties">
                 <div className="ratings">
                   <p>{restaurantData.rating}</p>
                 </div>
+                <p className="dot">●</p>
                 <p className="cuisine">
-                  {restaurantData.categories[0].title},
-                  {restaurantData.categories[1].title},
-                  {restaurantData.categories[2].title}
+                  {restaurantData.categories[0].title + ", "}
+                  {restaurantData.categories[1].title  + ", "}
+                  {restaurantData.categories[2].title + ", "}
                 </p>
+                <p className="dot">●</p>
                 <p className="cost">{restaurantData.price}</p>
+                <p className="dot">●</p>
                 <div className="phone-container">
                   <p className="phone-icon">📞</p>
                   <p className="phone-number">{restaurantData.display_phone}</p>
                 </div>
               </section>
-              <p className="address">
-                {restaurantData.location.display_address[0]},
-                {restaurantData.location.display_address[1]}
-              </p>
+              <div className="info-splitter"></div>
+              <section className="address-container">
+                <p className="address">
+                  {restaurantData.location.display_address[0] + ", "}{restaurantData.location.display_address[1]}
+                </p> 
+              </section>
             </section>
           </section>
+          <div className="section-splitter"></div>
           <section className="map-container"></section>
         </section>
         <button onClick={() => setRestaurantData(data2)} className="submit">
           New Random Restaurant
         </button>
-      </section>
+      </main>
     );
   }
 };
