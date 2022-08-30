794e6c498f70c0c17e1d2c92e73cfb9c98e07ca1
Dylan Duke <laytonmaes@gmail.com>
2022-08-29 20:21:31 -0600

    add footer, footer scss, and data for buttons

---
+++ src/Components/Footer.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <section className="footer">
+            <h3>Contact Us</h3>
+            <div className="project-lead">
+                <p className="footer-text">Luke Swenson</p>
+                <div className="footer-info">
+                <p className="footer-text">lukeswenson95@outlook.com</p>
+                <p className="footer-text">https://github.com/LukeSwenson06</p>
+                </div>
+            </div>
+            <section className="team">
+                <div className="team-member">
+                    <p className="footer-text">Becky Nisttahuz</p>
+                    <div className="footer-info">
+                        <p className="footer-text">benistta014@gmail.com</p>
+                        <p className="footer-text">https://github.com/benistta</p>
+                    </div>
+                </div>
+                <div className="team-member">
+                    <p className="footer-text">Grant X Beard</p>
+                    <div className="footer-info">
+                        <p className="footer-text">grantxbeard@gmail.com</p>
+                        <p className="footer-text">https://github.com/grantxbeard</p>
+                    </div>
+                </div>
+                <div className="team-member">
+                    <p className="footer-text">Lourdes Benites</p>
+                    <div className="footer-info">
+                        <p className="footer-text">lourdesben95@gmail.com</p>
+                        <p className="footer-text">https://github.com/lourdesbnts</p>
+                    </div>
+                </div>
+                <div className="team-member">
+                    <p className="footer-text">Kristy Nguyen</p>
+                    <div className="footer-info">
+                        <p className="footer-text">kristypnguyen7@gmail.com</p>
+                        <p className="footer-text">https://github.com/kpn678</p>
+                    </div>
+                </div>
+                <div className="team-member">
+                    <p className="footer-text">Jim Riddle</p>
+                    <div className="footer-info">
+                        <p className="footer-text">jimriddle0@gmail.com</p>
+                        <p className="footer-text">https://github.com/jimriddle1</p>
+                    </div>
+                </div>
+                <div className="team-member">
+                    <p className="footer-text">Dylan Duke</p>
+                    <div className="footer-info">
+                        <p className="footer-text">laytonmaes@gmail.com</p>
+                        <p className="footer-text">https://github.com/laytonmaes</p>
+                    </div>
+                </div>
+            </section>
+        </section>
+    )
+}
