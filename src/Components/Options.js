7343f92e097f4784de97aef615da18857583ca8b
Lourdes Benites <lourdesben95@gmail.com>
2022-08-31 16:48:41 -0600

    Change cuisine button selection and functionality behavior

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -2,15 +2,18 @@ import React from "react";
 import "../Styles/global.scss"
 import options from "../options_data";
 
-const Options = () => {
-const optionButtons = options.map((option, index) => {
-  return <button key={index} className="option-unchecked">{option}</button>
-})
-    return (
-        <section className="options-container">
-          {optionButtons}
-        </section>
-    )
+const Options = ({ cuisine, setCuisine }) => {
+  const optionButtons = options.map((option, index) => {
+    if (cuisine === option) {
+      return <button key={index} className="option-checked" id={option}>{option}</button>
+    }
+    return <button key={index} className="option-unchecked" id={option} onClick={(event) => {setCuisine(event.target.id)}}>{option}</button>
+  })
+  return (
+    <section className="options-container">
+      {optionButtons}
+    </section>
+  )
 }
 
-export default Options
+export default Options;
