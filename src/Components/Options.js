8abc0c73914d42b64ec997cb2d9da52c8bc7fd6c
Lourdes Benites <lourdesben95@gmail.com>
2022-08-31 16:27:52 -0600

    Add map for option buttons

--- src/Components/Options.js
+++ src/Components/Options.js
@@ -1,22 +1,14 @@
 import React from "react";
 import "../Styles/global.scss"
+import options from "../options_data";
 
 const Options = () => {
+const optionButtons = options.map((option, index) => {
+  return <button key={index} className="option-unchecked">{option}</button>
+})
     return (
         <section className="options-container">
-            <button className="option-checked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
-            <button className="option-unchecked">Test1</button>
+          {optionButtons}
         </section>
     )
 }
