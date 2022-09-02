21fb831a0d74226a02d54a2e8c00634f08774583
Grant Beard <Grantxbeard@gmail.com>
2022-09-02 16:31:52 -0600

    update API call with categories endpoint

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,5 +1,5 @@
-const getRestaurant = (lat, long) => {
-  return fetch(`https://foodive-be.herokuapp.com/restaurants/?location=${lat},${long}`)
+const getRestaurant = (lat, long, categories) => {
+  return fetch(`https://foodive-be.herokuapp.com/restaurants/?location=${lat},${long}&categories=${categories}`)
   .then((response) => {
     if (!response.ok) {
       throw new Error("Sorry, something went wrong!")
