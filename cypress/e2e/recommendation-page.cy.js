a1c5c0e27d87f5e5399bf0f6e6b36b3a07e45d9a
Lourdes Benites <lourdesben95@gmail.com>
2022-09-05 16:07:41 -0600

    change recommendation to cy visit

--- cypress/e2e/recommendation-page.cy.js
+++ cypress/e2e/recommendation-page.cy.js
@@ -18,10 +18,8 @@ describe('Recommendation page flow', () => {
       fixture: "restaurant1.json",
       statusCode: 200
     })
-    cy.visit("http://localhost:3000/", mockLocation(47.6062, -122.3321))
-    cy.wait(10000)
-    cy.get('.options-container').get('button').contains("Breakfast").click()
-    cy.get('.submit').click()
+    cy.visit("http://localhost:3000/recommendation/breakfast_brunch", mockLocation(47.6062, -122.3321))
+    cy.wait(5000)
   })
 
   it('Should take user to new page', () => {
