fef7909eab3ddb2d785e20375f0b4e9fd69cd774
Kristy Nguyen <kristypnguyen7@gmail.com>
2022-09-07 11:15:57 -0600

    Add a tags

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,59 +6,59 @@ const Footer = () => {
             <h3>Contact Us</h3>
             <div className="section-splitter"></div>
             <div className="project-lead">
-                <p className="footer-text">Luke Swenson</p>
+                <a className="footer-text" href='https://www.linkedin.com/in/luke-swenson' target='_blank' rel='noopener noreferrer'>Luke Swenson</a>
                 <div className="footer-info lead">
-                <p className="footer-text">lukeswenson95@outlook.com</p>
-                <p className="footer-text">https://github.com/LukeSwenson06</p>
+                <a className="footer-text" href = "mailto: lukeswenson95@outlook.com">lukeswenson95@outlook.com</a>
+                <a className="footer-text" href='https://github.com/LukeSwenson06' target='_blank' rel='noopener noreferrer'>https://github.com/LukeSwenson06</a>
                 </div>
             </div>
             <div className="section-splitter"></div>
             <section className="team">
                 <div className="team-member">
-                    <p className="footer-text">Becky Nisttahuz</p>
+                    <a className="footer-text" href='https://www.linkedin.com/in/grant-x-beard/' target='_blank' rel='noopener noreferrer'>Grant X Beard</a>
                     <div className="footer-info">
-                        <p className="footer-text">benistta014@gmail.com</p>
-                        <p className="footer-text">https://github.com/benistta</p>
+                        <a className="footer-text" href = "mailto: grantxbeard@gmail.com">grantxbeard@gmail.com</a>
+                        <a className="footer-text" href='https://github.com/grantxbeard' target='_blank' rel='noopener noreferrer'>https://github.com/grantxbeard</a>
                     </div>
                     <div className="section-splitter"></div>
                 </div>
                 <div className="team-member">
-                    <p className="footer-text">Grant X Beard</p>
+                    <a className="footer-text" href='https://www.linkedin.com/in/lourdesbenites/' target='_blank' rel='noopener noreferrer'>Lourdes Benites</a>
                     <div className="footer-info">
-                        <p className="footer-text">grantxbeard@gmail.com</p>
-                        <p className="footer-text">https://github.com/grantxbeard</p>
+                        <a className="footer-text" href = "mailto: lourdesben95@gmail.com">lourdesben95@gmail.com</a>
+                        <a className="footer-text" href='https://github.com/lourdesbnts' target='_blank' rel='noopener noreferrer'>https://github.com/lourdesbnts</a>
                     </div>
                     <div className="section-splitter"></div>
                 </div>
                 <div className="team-member">
-                    <p className="footer-text">Lourdes Benites</p>
+                    <a className="footer-text" href='https://www.linkedin.com/in/dylan-duke-005756129/' target='_blank' rel='noopener noreferrer'>Dylan Duke</a>
                     <div className="footer-info">
-                        <p className="footer-text">lourdesben95@gmail.com</p>
-                        <p className="footer-text">https://github.com/lourdesbnts</p>
+                        <a className="footer-text" href = "mailto: laytonmaes@gmail.com">laytonmaes@gmail.com</a>
+                        <a className="footer-text" href='https://github.com/laytonmaes' target='_blank' rel='noopener noreferrer'>https://github.com/laytonmaes</a>
                     </div>
                     <div className="section-splitter"></div>
                 </div>
                 <div className="team-member">
-                    <p className="footer-text">Kristy Nguyen</p>
+                    <a className="footer-text" href='https://www.linkedin.com/in/becky-nisttahuz/' target='_blank' rel='noopener noreferrer'>Becky Nisttahuz</a>
                     <div className="footer-info">
-                        <p className="footer-text">kristypnguyen7@gmail.com</p>
-                        <p className="footer-text">https://github.com/kpn678</p>
+                        <a className="footer-text" href = "mailto: benistta014@gmail.com">benistta014@gmail.com</a>
+                        <a className="footer-text" href='https://github.com/benistta' target='_blank' rel='noopener noreferrer'>https://github.com/benistta</a>
                     </div>
                     <div className="section-splitter"></div>
                 </div>
                 <div className="team-member">
-                    <p className="footer-text">Jim Riddle</p>
+                    <a className="footer-text" href='https://www.linkedin.com/in/kristypnguyen/' target='_blank' rel='noopener noreferrer'>Kristy Nguyen</a>
                     <div className="footer-info">
-                        <p className="footer-text">jimriddle0@gmail.com</p>
-                        <p className="footer-text">https://github.com/jimriddle1</p>
+                        <a className="footer-text" href = "mailto: kristypnguyen7@gmail.com">kristypnguyen7@gmail.com</a>
+                        <a className="footer-text" href='https://github.com/kpn678' target='_blank' rel='noopener noreferrer'>https://github.com/kpn678</a>
                     </div>
                     <div className="section-splitter"></div>
                 </div>
                 <div className="team-member">
-                    <p className="footer-text">Dylan Duke</p>
+                    <a className="footer-text" href='https://www.linkedin.com/in/jim-riddle-b6718037/' target='_blank' rel='noopener noreferrer'>Jim Riddle</a>
                     <div className="footer-info">
-                        <p className="footer-text">laytonmaes@gmail.com</p>
-                        <p className="footer-text">https://github.com/laytonmaes</p>
+                        <a className="footer-text" href = "mailto: jimriddle0@gmail.com">jimriddle0@gmail.com</a>
+                        <a className="footer-text" href='https://github.com/jimriddle1' target='_blank' rel='noopener noreferrer'>https://github.com/jimriddle1</a>
                     </div>
                     <div className="section-splitter"></div>
                 </div>
